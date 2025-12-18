35e4239552ed3b83406a665279284b5745743964
Mrudul Lole <mrudullole08@gmail.com>
2025-12-18 11:36:33 +0530

    Add variables for EC2 module configuration

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,14 @@
+variable "ami" {
+  description = "AMI ID for EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
