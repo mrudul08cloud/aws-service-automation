f8ce5c1407f4fe1234bcfa59945be5e11bfee00c
Mrudul Lole <mrudullole08@gmail.com>
2025-12-18 11:43:10 +0530

    Create variables.tf

---
+++ modules/s3/variables.tf
@@ -0,0 +1,9 @@
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
+
+variable "bucket_tag_name" {
+  description = "Name tag for the S3 bucket"
+  type        = string
+}
